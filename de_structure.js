8dcd23c2a3f2257428199483fdcf175cb41c2ccf
Anshum Shukla <anshumshukla003@gmail.com>
2023-12-11 23:11:30 +0530

    api_structure

---
+++ de_structure.js
@@ -0,0 +1,16 @@
+const course ={
+    name : "React",
+    price :999,
+    teacher :"hitesh"
+
+}
+console.log(course.teacher);
+const {name} =course
+console.log(name);
+
+// json example1
+{
+
+
+
+}
