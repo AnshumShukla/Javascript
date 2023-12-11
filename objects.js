9658b5a31bf281cd1493b6018d92d265827863b3
Anshum Shukla <anshumshukla003@gmail.com>
2023-12-11 22:16:46 +0530

    concept of obeject in javascript

---
+++ objects.js
@@ -0,0 +1,21 @@
+//  through constructor = singleton
+
+// object literals
+const sym = Symbol("key1")
+const sym2 = Symbol("key2")
+const user = {
+    name : "anshum",
+    sym : "kaka",
+    age : 20, 
+    [sym2]: "kaka1",
+    college :" abesit",
+    "friend": "pranav"
+}
+console.log(user.friend);
+console.log( typeof user.sym);
+console.log( typeof user.sym2);
+ user.fun = function () {
+    console.log(`hello MR. , ${this.name}`);
+    
+ }
+ console.log(user.fun());
