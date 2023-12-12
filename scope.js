614b2ce7aff5f3032a339194776e17f2ad246089
Anshum Shukla <anshumshukla003@gmail.com>
2023-12-12 19:35:49 +0530

    scope in javascript

---
+++ scope.js
@@ -0,0 +1,12 @@
+if (true) {
+    var a =10
+    let b =30
+    const c = 50
+    console.log("inner:",c);
+}
+ var a = 39;
+ const b = 40
+console.log(a);
+console.log(b);
+// console.log(c);
+// therefore var is not used because of the accessibility in the code
