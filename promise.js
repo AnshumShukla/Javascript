3a4bb261912d0adb7abfb22359cc797304c771e5
Anshum Shukla <anshumshukla003@gmail.com>
2023-12-19 22:49:07 +0530

    promise

---
+++ promise.js
@@ -0,0 +1,11 @@
+const promiseone = new Promise (function(resolve,reject){
+    setTimeout(function(){
+        console.log("Async is complete");
+        resolve()
+    },9000)
+  
+})
+  
+promiseone.then(function(){
+ console.log("promise is consumed");
+})
