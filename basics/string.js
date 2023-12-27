93543bdfea674b59484b59bd8f19c6845f765685
Anshum Shukla <anshumshukla003@gmail.com>
2023-12-27 22:59:08 +0530

    okay

--- basics/string.js
+++ basics/string.js
@@ -3,6 +3,5 @@
   console.log(`hello my name is ${name} and my age is ${age}`);
   const gamename = new String('ansh')
  console.log(gamename.toUpperCase());
- 
-  const school = new String("     abesit college of engineering")
+ const school = new String("     abesit college of engineering")
 console.log(`the length of the string is ${school.trimStart()}`);
